1f5ac3f709a6408cd557ea44c1f1bbe4769dc336
Hou Wenlong <houwenlong.hwl@antgroup.com>
2022-12-27 14:35:58 +0800

    x86/stackprotector/64: Use per-cpu stack canary if supported by compiler
    GCC 8.1 added options that allow the stack canary to be accessed as
    "%gs:__stack_chk_guard", effectively turning it into an ordinary per-cpu
    variable. For CONFIG_SMP=n, it is not necessary to set up GSBASE as it
    could be set as 0. So use the per-cpu stack canary if supported by the
    compiler and keep the fixed location code for compatibility with older
    compilers.
    
    [gavin: - conflict with 577c134d311b x86/stackprotector: Work around
    strict Clang TLS symbol requirements
    - change __stack_chk_guard to __ref_stack_chk_guard in arch/x86/Makefile
    - add #endif to terminate CONFIG_STACKPROTECTOR]
    
    Co-developed-by: Brian Gerst <brgerst@gmail.com>
    Signed-off-by: Brian Gerst <brgerst@gmail.com>
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>
    Signed-off-by: Gavin Guo <gavinguo@igalia.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -2079,10 +2079,6 @@ EXPORT_PER_CPU_SYMBOL(pcpu_hot);
 EXPORT_PER_CPU_SYMBOL(const_pcpu_hot);
 
 #ifdef CONFIG_X86_64
-DEFINE_PER_CPU_FIRST(struct fixed_percpu_data,
-		     fixed_percpu_data) __aligned(PAGE_SIZE) __visible;
-EXPORT_PER_CPU_SYMBOL_GPL(fixed_percpu_data);
-
 static void wrmsrl_cstar(unsigned long val)
 {
 	/*
@@ -2146,16 +2142,20 @@ void syscall_init(void)
 		idt_syscall_init();
 }
 
-#else	/* CONFIG_X86_64 */
+#endif	/* CONFIG_X86_64 */
 
 #ifdef CONFIG_STACKPROTECTOR
+#ifdef CONFIG_STACKPROTECTOR_FIXED
+DEFINE_PER_CPU_FIRST(struct fixed_percpu_data,
+		     fixed_percpu_data) __aligned(PAGE_SIZE) __visible;
+EXPORT_PER_CPU_SYMBOL_GPL(fixed_percpu_data);
+#else
 DEFINE_PER_CPU(unsigned long, __stack_chk_guard);
 #ifndef CONFIG_SMP
 EXPORT_PER_CPU_SYMBOL(__stack_chk_guard);
 #endif
 #endif
-
-#endif	/* CONFIG_X86_64 */
+#endif
 
 static void initialize_debug_regs(void)
 {
