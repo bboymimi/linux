24255f967338cf37d0da539d79bbf651d960f871
Lai Jiangshan <jiangshan.ljs@antgroup.com>
2024-01-09 20:17:54 +0800

    KVM: x86: Introduce vendor feature to expose vendor-specific CPUID
    For the PVM guest, it needs to detect PVM support early, even before IDT
    setup, so the cpuid instruction is used. Moreover, in order to
    differentiate PVM from VMX/SVM, a new CPUID is introduced to expose
    vendor-specific features. Currently, only PVM uses it.
    
    [gavin: fix the conflict by moving the cpuid vendor feature after
    kvm_init_xstate_sizes]
    
    Signed-off-by: Lai Jiangshan <jiangshan.ljs@antgroup.com>
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>
    Signed-off-by: Gavin Guo <gavinguo@igalia.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -56,6 +56,16 @@ void __init kvm_init_xstate_sizes(void)
 	}
 }
 
+u32 kvm_cpuid_vendor_features;
+EXPORT_SYMBOL_GPL(kvm_cpuid_vendor_features);
+u32 kvm_cpuid_vendor_signature;
+EXPORT_SYMBOL_GPL(kvm_cpuid_vendor_signature);
+
+static inline bool has_kvm_cpuid_vendor_features(void)
+{
+	return !!kvm_cpuid_vendor_signature;
+}
+
 u32 xstate_required_size(u64 xstate_bv, bool compacted)
 {
 	int feature_bit = 0;
@@ -1221,7 +1231,10 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 	}
 	case KVM_CPUID_SIGNATURE: {
 		const u32 *sigptr = (const u32 *)KVM_SIGNATURE;
-		entry->eax = KVM_CPUID_FEATURES;
+		if (!has_kvm_cpuid_vendor_features())
+			entry->eax = KVM_CPUID_FEATURES;
+		else
+			entry->eax = KVM_CPUID_VENDOR_FEATURES;
 		entry->ebx = sigptr[0];
 		entry->ecx = sigptr[1];
 		entry->edx = sigptr[2];
@@ -1246,6 +1259,17 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 			entry->eax |= (1 << KVM_FEATURE_STEAL_TIME);
 
 		entry->ebx = 0;
+		entry->ecx = 0;
+		entry->edx = 0;
+		break;
+	case KVM_CPUID_VENDOR_FEATURES:
+		if (!has_kvm_cpuid_vendor_features()) {
+			entry->eax = 0;
+			entry->ebx = 0;
+		} else {
+			entry->eax = kvm_cpuid_vendor_features;
+			entry->ebx = kvm_cpuid_vendor_signature;
+		}
 		entry->ecx = 0;
 		entry->edx = 0;
 		break;
